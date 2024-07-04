2978b0f7ec9fc854a391d8e4b3ec16948b9f8bb8
HonurX <honoruno22@gmail.com>
2024-07-04 10:14:01 -0400

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,68 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    id ("kotlin-kapt")
-    id ("androidx.navigation.safeargs.kotlin")
-    id("com.google.gms.google-services")
-}
-
-android {
-    namespace = "com.photosharingapp"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.photosharingapp"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        viewBinding = true
-    }
-
-    dependencies {
-
-        implementation(libs.androidx.core.ktx)
-        implementation(libs.androidx.appcompat)
-        implementation(libs.material)
-        implementation(libs.androidx.activity)
-        implementation(libs.androidx.constraintlayout)
-        testImplementation(libs.junit)
-        androidTestImplementation(libs.androidx.junit)
-        androidTestImplementation(libs.androidx.espresso.core)
-        implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
-        implementation("com.google.firebase:firebase-analytics")
-
-        val nav_version = "2.7.7"
-        // Kotlin
-        implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
-        implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
-
-        implementation("com.google.firebase:firebase-storage")
-        implementation("com.google.firebase:firebase-auth:23.0.0")
-        implementation("com.google.firebase:firebase-firestore")
-        implementation("com.squareup.picasso:picasso:2.71828")
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
 
 
-    }
 }
